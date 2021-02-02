49352f540a98aeb3bb1df65aceaac770b8479845
Himanshu Kumar <48569211+Himanshu396@users.noreply.github.com>
2021-02-02 11:31:55 +0530

    simple example of slice in go

---
+++ slice1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//Package scope array definition
+var integerSlice []int
+
+var stringSlice []string
+
+func main() {
+
+	integerSlice = []int{10, 20, 30, 40}
+
+	fmt.Println("This is the integer Slice: ", integerSlice)
+
+	stringSlice = []string{"first", "second", "third"}
+
+	fmt.Println("This is the string slice: ", stringSlice)
+
+	printInteger()
+
+}
+
+//This function can access the integerSlice because integerSlice is package scoped
+func printInteger() {
+	fmt.Println("Print the integers: ", integerSlice)
+}
